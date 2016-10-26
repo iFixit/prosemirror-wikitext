2bef2ce6e898d5cbbf722be195362cb55ab21bd7
Chris Opperwall <chriso@ifixit.com>
2016-10-25 21:38:41 -0700

    Add serializer support for code_block and headings
    This adds serializer rules for code_blocks and headings from level 2 to
    6. This also abstracts the logic used to render marks on inline text in
    paragraph nodes into a general function that works on text within
    headings, code_blocks, and list items.

--- src/to_wiki.js
+++ src/to_wiki.js
@@ -36,6 +36,38 @@ class WikiTextSerializerState {
       this.nodes[node.type.name](this, node)
    }
 
+   // Render inline text with their marks.
+   inline(node) {
+      let openMarks = []
+
+      let handleMarks = (node) => {
+         let marks = node.marks || []
+
+         // If there are marks that aren't in the openMarks list, then apply
+         // them, this is where they start.
+         //
+         // Also add them to active.
+         //
+         // Mark train has no breaks.
+         let toClose = openMarks.filter(mark => marks.indexOf(mark) < 0)
+         this.closeMarks(toClose.reverse())
+         openMarks = openMarks.filter(mark => toClose.indexOf(mark) < 0)
+
+         // new nodes are in marks, but not in openMarks
+         let toOpen = marks.filter(mark => openMarks.indexOf(mark) < 0)
+         openMarks = toOpen.concat(openMarks)
+
+         this.openMarks(toOpen)
+
+         this.render(node)
+         // If there are any active marks that are no in the marks variable, it
+         // is time to close them.
+      }
+
+      node.forEach(handleMarks)
+      this.closeMarks(openMarks.reverse())
+   }
+
    closeMarks(marks) {
       marks.forEach(mark => {
          this.out += this.marks[mark.type.name].close
@@ -52,49 +84,33 @@ class WikiTextSerializerState {
 const serializer = new WikiTextSerializer({
    // Nodes
    paragraph(state, node) {
-      // For now this basically needs to make sure that this starts on a
-      // newline and ends with a newline, and render whatever is within it.
       if (!/(^|\n)$/.test(state.out))
          state.out += "\n\n"
 
-      let openMarks = []
-
-      let handleMarks = (node) => {
-         let marks = node.marks || []
-
-         // If there are marks that aren't in the openMarks list, then apply
-         // them, this is where they start.
-         //
-         // Also add them to active.
-         //
-         // Mark train has no breaks.
-         let toClose = openMarks.filter(mark => marks.indexOf(mark) < 0)
-         state.closeMarks(toClose.reverse())
-         openMarks = openMarks.filter(mark => toClose.indexOf(mark) < 0)
-
-         // new nodes are in marks, but not in openMarks
-         let toOpen = marks.filter(mark => openMarks.indexOf(mark) < 0)
-         openMarks = toOpen.concat(openMarks)
-
-         state.openMarks(toOpen)
-
-         state.render(node)
-         // If there are any active marks that are no in the marks variable, it
-         // is time to close them.
-      }
-
-      node.forEach(handleMarks)
-      state.closeMarks(openMarks.reverse())
+      state.inline(node);
 
       state.out += "\n\n"
    },
-   text(state, node) {
-      // TODO: This uses  state.text, which I'm not what that does
 
+   text(state, node) {
       state.out += node.text
    },
+
    hard_break(state, node) {
       state.out += "[br]\n"
+   },
+
+   heading(state, node) {
+      let headerTag = "=".repeat(node.attrs.level)
+      state.out += headerTag + " "
+      state.inline(node);
+      state.out += " " + headerTag + "\n"
+   },
+
+   code_block(state, node) {
+      state.out += "[code]\n"
+      state.inline(node)
+      state.out += "\n[/code]\n"
    }
 }, {
    // Marks
